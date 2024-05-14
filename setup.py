c86ae9951e4df71e5b495cb2e1e22838844290db
Anubhav Yadav <ay6666@srmist.edu.in>
2024-05-14 11:52:50 +0530

    Update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name='Spam Classification',
+    name='Spam Classification Project',
     version='0.0.1',
     author='Anubhav Yadav',
     author_email='ay6666@srmist.edu.in',
